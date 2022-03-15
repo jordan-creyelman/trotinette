f781b9a9e364dcd7edfcbe09723ed288b80a3b63
hary rabarijoely <rabonia@hotmail.com>
2022-03-15 15:28:41 +0100

    update pages content

--- src/pages/SignOut/index.jsx
+++ src/pages/SignOut/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUserContext } from '../../UserContext';
+import Cookies from "js-cookie";
 import "./index.css"
 
 function SignOut() {
+    let navigate = useNavigate();
+  const {setUser} = useUserContext();
+
+  
+  const handleSubmit = () => {
+    Cookies.remove("token");          
+    setUser(null)
+    navigate("/");
+  };
+
   return (
     <div className='sign_out' >
-        <h1>SignOut</h1>
+        <button onClick={() => handleSubmit()} text={"Veux-tu vraiment te déconnecter ?"}>Déconnexion</button>
     </div>
   )
 }
