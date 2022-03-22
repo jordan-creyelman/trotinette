839ef9d30b72de645a391bbb9fd688860c5cbbcd
JulienRouet <julienrouet@live.fr>
2022-03-22 20:36:02 +0100

    update_a

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -10,11 +10,11 @@ export default function Contact() {
 
       <Container className='linkgithub'>
         <div className='link'>
-        <a href="https://github.com/jordan-creyelman">Jordan Creyelman</a>
-        <a href="https://github.com/HaryRabarijoely">Hary Andrianarisoa</a>
-        <a href="https://github.com/kalu6334">Lucas Martin</a>
-        <a href="https://github.com/Nicoclos">Nicolas Bia</a>
-        <a href="https://github.com/JulienRouet">Julien ROUET</a>
+        <a className='contactlink' href="https://github.com/jordan-creyelman">Jordan Creyelman</a>
+        <a className='contactlink' href="https://github.com/HaryRabarijoely">Hary Andrianarisoa</a>
+        <a className='contactlink' href="https://github.com/kalu6334">Lucas Martin</a>
+        <a className='contactlink' href="https://github.com/Nicoclos">Nicolas Bia</a>
+        <a className='contactlink' href="https://github.com/JulienRouet">Julien ROUET</a>
         </div>
         <img style={{height: 400, width: 800, borderRadius: 0,}}src={img3} alt="Banner" className='img-fluid'></img>
       </Container>
